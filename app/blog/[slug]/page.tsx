e4b2e268d4f593a3b91eefb86bf2e9b40aff1b54
Usman Ishaq <147960149+UsmanIshaq124@users.noreply.github.com>
2026-07-22 12:26:09 +0500

    Updated blog post page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -129,12 +129,10 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
                 </div>
 
                 <div className="flex flex-col">
-                  <div className="text-sm text-white font-semibold leading-none">
-                    {post.author_name || "Usman Ishaq"}
-                  </div>
+                 
 
                   <div className="text-xs text-white/60 mt-1">
-                    Author, Semantic SEO Strategist
+                    Founder & Lead Architect
                   </div>
                 </div>
               </div>
